5b36f6826e87e2668a76ebe40f81ab20b99efc9a
marwan <marwanabdalla1999@gmail.com>
2025-06-04 12:33:02 +0300

    fix min sdk

--- buildSrc/src/main/kotlin/VersionManger.kt
+++ buildSrc/src/main/kotlin/VersionManger.kt
@@ -2,6 +2,6 @@ object VersionManger {
     const val VERSION_NAME = "1.0.0"
     const val VERSION_CODE = 1
     const val COMPILE_SDK = 35
-    const val MIN_SDK = 24
+    const val MIN_SDK = 29
     const val TARGET_SDK = 35
 }
